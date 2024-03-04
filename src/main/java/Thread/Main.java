3039a98fdefe9fcef1ed0115b32177ffef533706
maxim shalashov <maluk@beelinerouter.net>
2024-03-04 15:23:20 +0600

    Init

--- src/main/java/Thread/Main.java
+++ src/main/java/Thread/Main.java
@@ -4,25 +4,56 @@ import java.util.ArrayList;
 
 public class Main
 {
+
+    public static class Th1 extends Thread
+    {
+        private ArrayList<Integer> array;
+
+        Th1(ArrayList<Integer> array)
+        {
+            this.array=array;
+        }
+        @Override
+        public void run() {
+            synchronized (array)
+            {
+                while (array.size()<=10000-1)
+                {
+                    array.add((int) (Math.random()*(10)));
+                }
+                System.out.println("1 array: "+array);
+            }
+        }
+    }
+
+    public static class Th2 extends Thread
+    {
+        private ArrayList<Integer> array;
+
+        Th2(ArrayList<Integer> array)
+        {
+            this.array=array;
+        }
+        @Override
+        public void run() {
+           synchronized (array)
+           {
+                while (!array.isEmpty())
+                {
+                    array.remove((int) (Math.random()*(array.size())));
+                }
+                System.out.println("2 array: "+array);
+           }
+        }
+    }
     public static void main(String[] strings) throws InterruptedException {
         // num 1
-        System.out.println(Thread.currentThread());
-
-        //num 2
-        //впринципе тут это нне надо, смотреть надо тест
-        Thread thread=new Thread(()->{System.out.println("myThread");});
-        thread.start();
-        thread.join();
-
-        //num 3
-        MyThread1 thread1=new MyThread1();
-        MyThread2 thread2=new MyThread2();
-        MyThread3 thread3=new MyThread3();
-        /*
-        thread1.start();thread1.join();
-        thread2.start();thread2.join();
-        thread3.start();thread3.join();
-        ArrayList<Integer> arrayList= new ArrayList<>();*/
-
+        //System.out.println(Thread.currentThread());
+        //num 4
+        ArrayList<Integer> array= new ArrayList<>();
+        Th1 th1=new Th1(array);
+        Th2 th2 = new Th2(array);
+        th1.start();
+        th2.start();
     }
 }
