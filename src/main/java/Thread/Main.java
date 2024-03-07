d8a3400ab45d377c7ad3f6bc2de3938bfcbe818b
maxShal <max_shalash@mail.ru>
2024-03-07 18:25:53 +0600

    Add 3,4

--- src/main/java/Thread/Main.java
+++ src/main/java/Thread/Main.java
@@ -8,52 +8,67 @@ public class Main
     public static class Th1 extends Thread
     {
         private ArrayList<Integer> array;
-
         Th1(ArrayList<Integer> array)
         {
             this.array=array;
         }
         @Override
-        public void run() {
-            synchronized (array)
-            {
-                while (array.size()<=10000-1)
+        public void run()
+        {
+            int i=0;
+                while (i<10000)
                 {
-                    array.add((int) (Math.random()*(10)));
+                    synchronized (array)
+                    {
+                        array.add((int) (Math.random()*(10)));
+                        System.out.println("Added: "+i);
+
+                        array.notify();
+                        i++;
+
+                    }
                 }
-                System.out.println("1 array: "+array);
-            }
+
         }
     }
 
     public static class Th2 extends Thread
     {
         private ArrayList<Integer> array;
-
         Th2(ArrayList<Integer> array)
         {
             this.array=array;
         }
         @Override
-        public void run() {
-           synchronized (array)
-           {
-                while (!array.isEmpty())
+        public void run()
+        {
+            for (int i=10000;i>0;i--)
+            {
+                synchronized (array)
                 {
+                    if(array.isEmpty())
+                    {
+                        try {
+                            array.wait();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
                     array.remove((int) (Math.random()*(array.size())));
+                    System.out.println("Deleted: "+i);
                 }
-                System.out.println("2 array: "+array);
-           }
+            }
         }
     }
     public static void main(String[] strings) throws InterruptedException {
-        // num 1
+        //num 1
         //System.out.println(Thread.currentThread());
         //num 4
         ArrayList<Integer> array= new ArrayList<>();
         Th1 th1=new Th1(array);
         Th2 th2 = new Th2(array);
-        th1.start();
         th2.start();
+        th1.start();
+
     }
 }
